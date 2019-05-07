301e7810ddac3563a8b9ce0ae76672c8261b321b
Abdulkadir Şen <abdulkadir.sen@neyasis.com>
2019-05-07 13:55:06 +0300

    initial create

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # micros
 
 Microservices demo with docker
+
+Base address localhost
+Base port: 10000
+
+Base path: localhost:10000
---
+++ src/micros.services.products/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace micros.services.products.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        // GET: api/Products
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Product 1", "Product 2" };
+        }
+
+        // GET: api/Products/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return $"Product {id}";
+        }
+    }
+}
